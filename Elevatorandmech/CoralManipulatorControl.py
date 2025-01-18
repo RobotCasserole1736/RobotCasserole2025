b542533f59d81db6dbfa137c19dc8e3135f10f5c
OryxDesktop <frc1736student@gmail.com>
2025-01-18 11:26:48 -0800

    skeleton code for yet unnamed coral manipulator

---
+++ Elevatorandmech/CoralManipulatorControl.py
@@ -0,0 +1,25 @@
+import wpilib
+import wpimath 
+from utils.singleton import Singleton
+
+class CoralManipulatorControl(metaclass=Singleton):
+
+    def __init__(self):
+        
+        pass 
+
+    def update(self):
+        pass
+
+    def enableIntake(self):
+        pass
+
+    def disableIntake(self):
+        pass 
+
+    def enableEject(self):
+        pass 
+
+    def disableEject(self):
+        pass 
+    
