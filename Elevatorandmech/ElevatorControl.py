49377e236a9c0e0ed3adbe8f1ec25e336970eab6
Traian86865649 <traian86865649@gmail.com>
2025-01-21 19:03:19 -0600

    updates to elevator

--- Elevatorandmech/ElevatorControl.py
+++ Elevatorandmech/ElevatorControl.py
@@ -1,5 +1,5 @@
 from playingwithfusion import TimeOfFlight
-from Elevatorandmech.ElevatorandMechConstants import MAX_ELEV_ACCEL_MPS2, MAX_ELEV_VEL_MPS
+from Elevatorandmech.ElevatorandMechConstants import MAX_ELEV_ACCEL_MPS2, MAX_ELEV_VEL_MPS, ELEV_GEARBOX_GEAR_RATIO, ELEV_SPOOL_RADIUS_M
 from utils.calibration import Calibration
 from utils.units import sign
 from utils.signalLogging import log
@@ -7,11 +7,11 @@ from utils.constants import ELEV_LM_CANID, ELEV_RM_CANID, ELEV_TOF_CANID
 from utils.singleton import Singleton
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 from rev import SparkBase, SparkBaseConfig, SparkMax, SparkLowLevel, SparkMaxConfig
+from Elevatorandmech.Profiler import ProfiledAxis
 
 # need to figure out a way to import
 class ElevatorControl():
     def __init__(self):
-        pass
         self.Rmotor = WrapperedSparkMax(ELEV_RM_CANID, "ElevatorMotor", brakeMode=True)
         #self.Rmotor.setInverted(True)
         self.LMotor = WrapperedSparkMax(ELEV_LM_CANID, SparkLowLevel.MotorType.kBrushless, brakeMode=True)
@@ -37,6 +37,55 @@ class ElevatorControl():
         self.Rmotor.setPID(self.kP.get(), 0.0, 0.0)
 
         self.stopped = True
+        # Absolute Sensor mount offsets
+        # After mounting the sensor, these should be tweaked one time
+        # in order to adjust whatever the sensor reads into the reference frame
+        # of the mechanism
+        self.absOffsetM = 0.074
 
-    def update(self):  
+        # Relative Encoder Offsets
+        # Releative encoders always start at 0 at power-on
+        # However, we may or may not have the mechanism at the "zero" position when we powered on
+        # These variables store an offset which is calculated from the absolute sensors
+        # to make sure the relative sensors inside the encoders accurately reflect
+        # the actual position of the mechanism
+        self.relEncOffsetM = 0.0
+
+        self.profiledPos = 0.0
+        self.curUnprofiledPosCmd = 0.0
+    def _RmotorRadToHeight(self, RmotorRad):
+        return RmotorRad * 1/ELEV_GEARBOX_GEAR_RATIO * (ELEV_SPOOL_RADIUS_M) - self.relEncOffsetM
+    def getHeightM(self):
+        return self._RmotorRadToHeight(self.Rmotor.getMotorPositionRad())
+    #def update(self):  
+        #update current sensor heights, etc. 
+        #pass
+    def update(self):
+        actualPos = self.getHeightM()
+
+        # Update motor closed-loop calibration
+        if(self.kP.isChanged()):
+            self.Rmotor.setPID(self.kP.get(), 0.0, 0.0)
+
+        if(self.stopped):
+            self.Rmotor.setVoltage(0.0)
+            self.profiledPos = actualPos
+        else:
+            motorPosCmd = self._heightToMotorRad(curState.position)
+            self.motorVelCmd = self._heightVeltoMotorVel(curState.velocity)
+
+            vFF = self.kV.get() * self.motorVelCmd  + self.kS.get() * sign(self.motorVelCmd) + self.kG.get()
+
+            self.Rmotor.setPosCmd(motorPosCmd, vFF)
+            self.LMotor.setFollow(self.Rmotor.ctrl, True)
+
+    def setHeightGoal(self, goal):
+        #self.heightGoal = goal
+        """
+        if goal == heightStates.L1:
+            self.heightGoal = L1_HEIGHT
+        elif _____:
+            ...
+        
+        """
         pass
--- Elevatorandmech/ElevatorandMechConstants.py
+++ Elevatorandmech/ElevatorandMechConstants.py
@@ -1,6 +1,8 @@
 #the max speed/acceleration the elevator can go
 from utils.units import in2m
 
-
+#All Numbers are placeholders for now (numbers used last year)
+ELEV_GEARBOX_GEAR_RATIO = 16.0/1.0
+ELEV_SPOOL_RADIUS_M = in2m(0.75)
 MAX_ELEV_VEL_MPS = in2m(24.0)
 MAX_ELEV_ACCEL_MPS2 = in2m(24.0)
