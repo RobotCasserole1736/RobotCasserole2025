1430d8c159cff13e7fd02a66edcc3c68d4334099
Alex Lutz <frc1736student@gmail.com>
2025-01-22 20:49:07 -0600

    not finished yet; still working on trapezoid profiler

--- Elevatorandmech/algeaManipulatorControl.py
+++ Elevatorandmech/algeaManipulatorControl.py
@@ -1,7 +1,8 @@
+from math import cos
 from Elevatorandmech.ElevatorandMechConstants import ALGAE_ANGLE_ABS_POS_ENC_OFFSET
 from utils.signalLogging import log
 import wpilib
-import wpimath 
+from wpimath.trajectory import TrapezoidProfile
 from utils import faults
 from utils.calibration import Calibration
 from utils.constants import ALGAE_INT_CANID, ALGAE_WRIST_CANID, ALGAE_GAMEPIECE_CANID, ALGAE_ENC_PORT
@@ -11,6 +12,7 @@ from utils.units import m2in, rad2Deg, deg2Rad
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 from playingwithfusion import TimeOfFlight
 from wrappers.wrapperedThroughBoreHexEncoder import WrapperedThroughBoreHexEncoder
+import math
 
 class AlgaeWristState(Enum):
     DISABLED = 0
@@ -24,18 +26,30 @@ class AlgaeWristControl(metaclass=Singleton):
     def __init__(self):
         #one important assumption we're making right now is that we don't need limits on the algae manipulator based on elevator height
 
+        self.kG = Calibration(name="Algae kG", default=0.1, units="V/degErr")
+        self.kS = Calibration(name="Algae kS", default=0.1, units="V/degErr")
+        self.kV = Calibration(name="Algae kV", default=0.1, units="V/degErr")
         self.kP = Calibration(name="Algae kP", default=0.1, units="V/degErr")
         self.maxOutputV = Calibration(name="Algae Max Voltage", default=12.0, units="V")
+
 
         self.WristCurState = AlgaeWristState.DISABLED
         self.wristMotor = WrapperedSparkMax(ALGAE_WRIST_CANID, "AlgaeWristMotor", True, 10)
         self.curMotorVoltage = 0.0
-        self.STOW_POS_DEG = 0 #CHANGE
-        self.INTAKE_POS_DEG = 112 #CHANGE
-        self.REEF_POS_DEG = 0.0 #CHANGE
+        self.STOW_POS_DEG = Calibration(name="Stow Position", default = 0, units="deg")
+        self.INTAKE_POS_DEG = Calibration(name="Intake Position", default = 0, units="deg")
+        self.REEF_POS_DEG = Calibration(name="Reef Position", default = 0, units="deg")
         self.ABSOLUTE_OFFSET = ALGAE_ANGLE_ABS_POS_ENC_OFFSET
-        self.curPosCmdDeg = self.STOW_POS_DEG
+        self.curPosCmdDeg = self.STOW_POS_DEG.get()
         self.algaeAbsEnc = WrapperedThroughBoreHexEncoder(port=ALGAE_ENC_PORT, name="AlgaeEncOffset", mountOffsetRad=deg2Rad(self.ABSOLUTE_OFFSET))
+        self.pos = AlgaeWristState
+        self.disPos = 0
+        self.inPos = 0
+        self.stowPos = 0
+        self.reefPos = 0
+        self.maxAcc = 0
+        self.maxVel = 0
+        self.vel = 0
         #set the degree details for your goals. For stow, intake off gorund, etc. 
         #Also, the absolute offset will be a constant that you can set here or just have in constants 
     
@@ -49,9 +63,28 @@ class AlgaeWristControl(metaclass=Singleton):
         
         #this is where you will figure out where you're trying to go. See tunerAngleControl.py for a simple answer
         #this could work if it's light enough. But you might have to go to something more like singerA
-        self.algaeAbsEnc.update()
+        if(self.pos == 0):
+            self.desPos = self.disPos
+        elif(self.pos == 1):
+            self.desPos = self.inPos
+        elif(self.pos == 2):
+            self.desPos = self.stowPos
+        elif(self.pos == 3):
+            self.desPos = self.reefPos
+        else:
+            pass
 
+        TrapezoidProfile.Constraints(self.maxVel, self.maxAcc)
+        TrapezoidProfile.State(self.desPos, self.vel)
+        self.profile = TrapezoidProfile(TrapezoidProfile.Constraints(self.maxVel, self.maxAcc))
+
+
+
+        self.algaeAbsEnc.update()
+        
         actualPos = self.getAngleDeg()
+
+        self.kG = math.cos(actualPos)
 
         err =  self.curPosCmdDeg - actualPos
         
@@ -70,12 +103,6 @@ class AlgaeWristControl(metaclass=Singleton):
         log("Algae Pos Act", actualPos ,"deg")
         log("Algae Motor Cmd", motorCmdV, "V")
         
-        
-     
-    def setGoal(self):
-        #here is where you will want to set which angle you're trying to go to. See 2024 tunerAngleControl.py
-        pass
-         
 class AlgeaIntakeControl(metaclass=Singleton):
 
     def __init__(self):
