cacf146f2f1df78fc62e3d98590be839e98bb469
Chris Gerth <chrisgerth010592@gmail.com>
2025-02-18 21:06:10 -0600

    Updated autosteer to transform to the correct alliance

--- drivetrain/controlStrategies/autoSteer.py
+++ drivetrain/controlStrategies/autoSteer.py
@@ -1,29 +1,21 @@
-import math
 from wpilib import Timer
-from wpimath.filter import SlewRateLimiter
-from wpimath.geometry import Pose2d, Rotation2d, Translation2d
+from wpimath.geometry import Pose2d, Rotation2d
 from drivetrain.drivetrainCommand import DrivetrainCommand
-from drivetrain.drivetrainPhysical import MAX_ROTATE_ACCEL_RAD_PER_SEC_2
-from utils.allianceTransformUtils import transformX
+from utils.allianceTransformUtils import transform
 from utils.calibration import Calibration
-from utils.constants import REEF_LOC_X_M, REEF_LOC_Y_M
-from utils.signalLogging import log
+from utils.constants import reefLocation
 from utils.singleton import Singleton
 
 class AutoSteer(metaclass=Singleton):
     def __init__(self):
         self.reefAlignActive = False
         self.returnDriveTrainCommand = DrivetrainCommand()
-        self.rotKp = Calibration("Auto Align Rotation Kp",3)
-        self.maxRotSpd = Calibration("Auto Align Max Rotate Speed", 4)
+        self.rotKp = Calibration("Auto Align Rotation Kp",5)
+        self.maxRotSpd = Calibration("Auto Align Max Rotate Speed", 6)
 
         # Previous Rotation Speed and time for calculating derivative
         self.prevDesAngle = 0
         self.prevTimeStamp = Timer.getFPGATimestamp()
-
-        # Set speaker coordinates
-        self.reefX = transformX(REEF_LOC_X_M)
-        self.reefY = REEF_LOC_Y_M
 
         self.desiredAngle = 0
 
@@ -42,7 +34,7 @@ class AutoSteer(metaclass=Singleton):
             return cmdIn
 
     def getRotationAngle(self, curPose: Pose2d) -> Rotation2d:
-        targetLocation = Translation2d(transformX(self.reefX),self.reefY)
+        targetLocation = transform(reefLocation)
         robotToTargetTrans = targetLocation - curPose.translation()
         return Rotation2d(robotToTargetTrans.X(), robotToTargetTrans.Y())
 
