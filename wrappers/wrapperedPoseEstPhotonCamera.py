c1eee165831b2e17c5357e4e67ae480a4ffac4e9
Chris Gerth <chrisgerth010592@gmail.com>
2025-03-13 18:16:49 -0500

    maaaaaaybe better

--- wrappers/wrapperedPoseEstPhotonCamera.py
+++ wrappers/wrapperedPoseEstPhotonCamera.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass
 import wpilib
 from wpimath.units import feetToMeters, degreesToRadians
 from wpimath.geometry import Pose2d
-from photonlibpy.photonCamera import PhotonCamera
+from wrappers.casserolePhotonCamera import PhotonCamera
 from photonlibpy.targeting.photonTrackedTarget import PhotonTrackedTarget
 from photonlibpy.photonCamera import setVersionCheckEnabled
 from utils.fieldTagLayout import FieldTagLayout
 from utils.faults import Fault
+from utils.signalLogging import addLog
 
 # Describes one on-field pose estimate from the a camera at a specific time.
 @dataclass
@@ -30,96 +31,110 @@ class WrapperedPoseEstPhotonCamera:
     def __init__(self, camName, robotToCam):
         setVersionCheckEnabled(False)
 
+        self.name = camName
         self.cam = PhotonCamera(camName)
 
         self.disconFault = Fault(f"Camera {camName} not sending data")
         self.timeoutSec = 1.0
         self.poseEstimates:list[CameraPoseObservation] = []
         self.robotToCam = robotToCam
+        self.lastLatency = 0.0
+        self.updateDuration = 0.0
+        addLog(f"PoseCam {self.name} latency", lambda:self.lastLatency, "sec")
+        addLog(f"PoseCam {self.name} Duration", lambda:self.updateDuration, "ms")
+        self.prevTimestampSec = 0.0
 
     def update(self, prevEstPose:Pose2d):
 
+        startTime = wpilib.Timer.getFPGATimestamp()
+
         self.poseEstimates = []
+        self.lastLatency = 0.0
 
         if(not self.cam.isConnected()):
             # Faulted - no estimates, just return.
             self.disconFault.setFaulted()
             return
 
-        # Grab whatever the camera last reported for observations in a camera frame
-        # Note: Results simply report "I processed a frame". There may be 0 or more targets seen in a frame
+        # Grab whatever the camera last reported for observations in all camera frames
         res = self.cam.getLatestResult()
 
-        # hack - results also have a more accurate "getTimestamp()", but this is
-        # broken in photonvision 2.4.2. Hack with the non-broken latency calcualtion
-        # TODO: in 2025, fix this to actually use the real latency
-        #latency = 0.05 # a total guess
-        obsTime = res.getTimestampSeconds()
-        
-        
+        if (res.getTimestampSeconds() != self.prevTimestampSec): # Only process if new timestamp
+            resIdx = 0
+            self.prevTimestampSec = res.getTimestampSeconds()
+            obsTime = res.getTimestampSeconds()
+            self.lastLatency = wpilib.Timer.getFPGATimestamp() - obsTime
+            self.disconFault.setNoFault()
 
-        # Update our disconnected fault since we have something from the camera
-        self.disconFault.setNoFault()
 
-        # Process each target.
-        # Each target has multiple solutions for where you could have been at on the field
-        # when you observed it
-        # (https://docs.wpilib.org/en/stable/docs/software/vision-processing/
-        # apriltag/apriltag-intro.html#d-to-3d-ambiguity)
-        # We want to select the best possible pose per target
-        # We should also filter out targets that are too far away, and poses which
-        # don't make sense.
-        for target in res.getTargets():
-            # Transform both poses to on-field poses
-            tgtID = target.getFiducialId()
-            if tgtID >= 0:
-                # Only handle valid ID's
-                tagFieldPose = FieldTagLayout().lookup(tgtID)
-                if tagFieldPose is not None:
-                    # Only handle known tags
-                    poseCandidates:list[Pose2d] = []
-                    if target.getPoseAmbiguity() <= .5:
-                        poseCandidates.append(
-                            self._toFieldPose(tagFieldPose, target.getBestCameraToTarget())
-                        )
-                        poseCandidates.append(
-                            self._toFieldPose(tagFieldPose, target.getAlternateCameraToTarget())
-                    )
+            # Process each target.
+            # Each target has multiple solutions for where you could have been at on the field
+            # when you observed it
+            # (https://docs.wpilib.org/en/stable/docs/software/vision-processing/
+            # apriltag/apriltag-intro.html#d-to-3d-ambiguity)
+            # We want to select the best possible pose per target
+            # We should also filter out targets that are too far away, and poses which
+            # don't make sense.
+            for tgtIdx, target in enumerate(res.getTargets()):
+                # Transform both poses to on-field poses
+                tgtID = target.getFiducialId()
 
-                    # Filter candidates in this frame to only the valid ones
-                    filteredCandidates:list[Pose2d] = []
-                    for poseCandidate in poseCandidates:
-                        isHorrible = tgtID in HUMAN_STATION_TAG_IDS
-                        onField = self._poseIsOnField(poseCandidate)
-                        closeEnough = self._closeEnoughToCamera(target)
-                        # Add other filter conditions here
-                        if onField and closeEnough and not isHorrible:
-                            filteredCandidates.append(poseCandidate)
+                if tgtID >= 0:
+                    # Only handle valid ID's
+                    tagFieldPose = FieldTagLayout().lookup(tgtID)
 
-                    # Pick the candidate closest to the last estimate
-                    bestCandidate:(Pose2d|None) = None
-                    bestCandidateDist = 99999999.0
-                    for poseCandidate in filteredCandidates:
-                        delta = (poseCandidate - prevEstPose).translation().norm()
-                        if delta < bestCandidateDist:
-                            # This candidate is better, use it
-                            bestCandidate = poseCandidate
-                            bestCandidateDist = delta
+                    if tagFieldPose is not None:
+                        # Only handle known tags
+                        poseCandidates:list[Pose2d] = []
+                        if target.getPoseAmbiguity() <= .5:
+                            poseCandidates.append(
+                                self._toFieldPose(tagFieldPose, target.getBestCameraToTarget())
+                            )
+                            poseCandidates.append(
+                                self._toFieldPose(tagFieldPose, target.getAlternateCameraToTarget())
+                            )
 
-                    # Finally, add our best candidate the list of pose observations
-                    if bestCandidate is not None:
-                        # TODO: we can probably get better standard deviations than just
-                        # assuming the default. Check out what 254 did in 2024:
-                        # https://github.com/Team254/FRC-2024-Public/blob/040f653744c9b18182be5f6bc51a7e505e346e59/src/main/java/com/team254/frc2024/subsystems/vision/VisionSubsystem.java#L381
-                        
-                        # First pass - we trust reef tags more
-                        stdDev = 1.0 if tgtID in REEF_TAG_IDS else 3.0
-                        
-                        self.poseEstimates.append(
-                            CameraPoseObservation(obsTime, 
-                                                  bestCandidate, 
-                                                  xyStdDev=stdDev)
-                        )
+
+                        # Filter candidates in this frame to only the valid ones
+                        filteredCandidates:list[Pose2d] = []
+                        for poseCandidate in poseCandidates:
+                            isHorrible = tgtID in HUMAN_STATION_TAG_IDS
+                            onField = self._poseIsOnField(poseCandidate)
+                            closeEnough = self._closeEnoughToCamera(target)
+                            # Add other filter conditions here
+                            if onField and closeEnough and not isHorrible:
+                                filteredCandidates.append(poseCandidate)
+
+
+                        # Pick the candidate closest to the last estimate
+                        bestCandidate:(Pose2d|None) = None
+                        bestCandidateDist = 99999999.0
+                        for poseCandidate in filteredCandidates:
+                            delta = (poseCandidate - prevEstPose).translation().norm()
+                            if delta < bestCandidateDist:
+                                # This candidate is better, use it
+                                bestCandidate = poseCandidate
+                                bestCandidateDist = delta
+
+
+                        # Finally, add our best candidate the list of pose observations
+                        if bestCandidate is not None:
+                            # TODO: we can probably get better standard deviations than just
+                            # assuming the default. Check out what 254 did in 2024:
+                            # https://github.com/Team254/FRC-2024-Public/blob/040f653744c9b18182be5f6bc51a7e505e346e59/src/main/java/com/team254/frc2024/subsystems/vision/VisionSubsystem.java#L381
+                            
+                            # First pass - we trust reef tags more
+                            stdDev = 1.0 if tgtID in REEF_TAG_IDS else 3.0
+                            
+                            self.poseEstimates.append(
+                                CameraPoseObservation(obsTime, 
+                                                    bestCandidate, 
+                                                    xyStdDev=stdDev)
+                            )
+
+
+        endTime = wpilib.Timer.getFPGATimestamp()
+        self.updateDuration = (endTime - startTime)*1000.0
 
     def getPoseEstimates(self):
         return self.poseEstimates
